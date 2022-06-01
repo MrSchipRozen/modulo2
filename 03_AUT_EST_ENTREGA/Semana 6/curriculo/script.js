ef7b49131bd76c198500b5fe3171497f4d7da88b
MrSchipRozen <99350292+MrSchipRozen@users.noreply.github.com>
2022-06-01 16:19:38 -0300

    Update Semana 6 curriculo

---
+++ 03_AUT_EST_ENTREGA/Semana 6/curriculo/script.js
@@ -0,0 +1,42 @@
+var element = document.getElementById('element');
+alert = ('Bem vindo ao curriculo')
+
+
+const express = require("express");
+const app = express();
+const hostname = "127.0.0.1";
+const port = 3011;
+const sqlite3 = require("sqlite3").verbose();
+const DBPATH = "BD.Curriculo.db";
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static("../frontcv/"));
+app.get("/users", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var db = new sqlite3.Database(DBPATH);
+  var sql = "SELECT * FROM Curriculo ORDER BY Curriculo COLLATE NOCASE";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+  });
+  db.close();
+});
+app.get("/users", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Isso é importante para evitar o erro de CORS
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = "SELECT * FROM Curriculo ORDER BY Curriculo COLLATE NOCASE";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+  });
+  db.close(); // Fecha o banco
+});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
